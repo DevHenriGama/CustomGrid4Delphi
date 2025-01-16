10a8bb5b4d69dfa707965cba406c559ffa555221
macsistemas4 <55088714+macsistemas4@users.noreply.github.com>
2025-01-16 10:38:00 -0300

    Update srcs

---
+++ src/CustomGrid.Types.pas
@@ -0,0 +1,15 @@
+unit CustomGrid.Types;
+
+interface
+
+uses
+  System.Generics.Collections, Vcl.Forms;
+
+type
+  TListItens = TObjectList<TFrame>;
+
+  TGridRowStyle = (gsInLine, gsInColum);
+
+implementation
+
+end.
